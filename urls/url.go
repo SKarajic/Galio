69e67be43f2dc16a4de806a1afa9d6ca36a9dedc
Sandi Karajic <sandibosna@outlook.com>
2018-03-31 21:36:22 +0200

    Adding some documentation

--- urls/url.go
+++ urls/url.go
@@ -3,5 +3,7 @@ package urls
 type URL string
 
 const (
+	// A formatting URL for the RiotGames league of legends API, the first value should be the region and the second
+	// region should be the endpoint
 	API = "https://%s.api.riotgames.com/lol/%v"
 )
